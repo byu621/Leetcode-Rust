f2a60f390605030acb89f3f835db77b3f14971bd
Benjamin Yu <byu621@aucklanduni.ac.nz>
2023-03-18 16:46:54 +1300

    two sum

--- src/problem/s0001_two_sum.rs
+++ src/problem/s0001_two_sum.rs
@@ -1,26 +1,35 @@
 pub struct Solution {}
 
+use std::collections::HashMap;
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        vec![]
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        for (index, num) in nums.iter().enumerate() {
+            let option = map.get(&(target - num));
+            match option {
+                None => {
+                    map.insert(num, index);
+                }
+                Some(t) => {
+                    return vec![*t as i32, index as i32];
+                }
+            }
+        }
+        return vec![];
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::Solution;
 
     #[test]
     fn test_1() {
-        assert_eq!(2, 2);
-    }
-
-    #[test]
-    fn test_2() {
         assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
     }
 
     #[test]
-    fn test_3() {
+    fn test_2() {
         assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
     }
 }
